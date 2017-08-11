04b8b94f2b80eccecbf736b9b1c9677990152d14
at055612 <at055612@gmail.com>
2017-08-11 13:17:16 +0100

    Change order and fix bug in script

--- upliftApiMajorVersion.sh
+++ upliftApiMajorVersion.sh
@@ -11,28 +11,28 @@ if [ "$#" -ne 2 ]; then
 fi
 
 oldMajorVersion=$1
-newMajorVersion=$1
+newMajorVersion=$2
 
 echo "Uplifting major version from $oldMajorVersion to $newMajorVersion"
-
-
-echo ""
-echo "Re-versioning ${PACKAGE_PREFIX_QUERY_API} packages"
-echo "About to change `find . -type f -name "*.java" | xargs grep "${PACKAGE_PREFIX_QUERY_API}.${oldMajorVersion}" | wc -l` lines"
-find . -type f -name "*.java" | xargs sed -i "s/${PACKAGE_PREFIX_QUERY_API}.${oldMajorVersion}/${PACKAGE_PREFIX_QUERY_API}.${newMajorVersion}/g"
-
-echo ""
-echo "Re-versioning ${PACKAGE_PREFIX_DATASOURCE_API} packages"
-echo "About to change `find . -type f -name "*.java" | xargs grep "${PACKAGE_PREFIX_DATASOURCE_API}.${oldMajorVersion}" | wc -l` lines"
-find . -type f -name "*.java" | xargs sed -i "s/${PACKAGE_PREFIX_DATASOURCE_API}.${oldMajorVersion}/${PACKAGE_PREFIX_DATASOURCE_API}.${newMajorVersion}/g"
 
 echo ""
 echo "Renaming package directories"
 for versionedDir in `find ./stroom-query-api/src -type d -name "*${oldMajorVersion}"`; do 
     newDir=`echo "${versionedDir}" | sed "s/${oldMajorVersion}$/${newMajorVersion}/"`
     echo "renaming directory ${versionedDir} to ${newDir}"; 
-    #mv "${versionedDir}" "${newDir}"
+    mv "${versionedDir}" "${newDir}"
 done
+
+echo ""
+echo "Re-versioning ${PACKAGE_PREFIX_QUERY_API} packages"
+echo "About to change `find . -type f -name "*.java" | xargs grep "${PACKAGE_PREFIX_QUERY_API}.${oldMajorVersion}" | wc -l` lines"
+find . -type f -name "*.java" | xargs sed -i "s/${PACKAGE_PREFIX_QUERY_API}\.${oldMajorVersion}/${PACKAGE_PREFIX_QUERY_API}.${newMajorVersion}/g"
+
+echo ""
+echo "Re-versioning ${PACKAGE_PREFIX_DATASOURCE_API} packages"
+echo "About to change `find . -type f -name "*.java" | xargs grep "${PACKAGE_PREFIX_DATASOURCE_API}.${oldMajorVersion}" | wc -l` lines"
+find . -type f -name "*.java" | xargs sed -i "s/${PACKAGE_PREFIX_DATASOURCE_API}\.${oldMajorVersion}/${PACKAGE_PREFIX_DATASOURCE_API}.${newMajorVersion}/g"
+
 
 
 
