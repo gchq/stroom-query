fed264d9f22d3afa16c2f23a20134f2746fb5b1b
at055612 <at055612@gmail.com>
2017-08-11 14:29:59 +0100

    Add replacement of package refewrences in query-common java code

--- upliftApiMajorVersion.sh
+++ upliftApiMajorVersion.sh
@@ -2,7 +2,8 @@
 
 PACKAGE_PREFIX_QUERY_API="stroom.query.api"
 PACKAGE_PREFIX_DATASOURCE_API="stroom.datasource.api"
-SRC_DIR="./stroom-query-api/src"
+SRC_DIR_API="./stroom-query-api/src"
+SRC_DIR_COMMON="./stroom-query-common/src"
 
 if [ "$#" -ne 2 ]; then
     echo "ERROR Invalid arguments" >&2
@@ -18,21 +19,31 @@ echo "Uplifting major version from $oldMajorVersion to $newMajorVersion"
 
 echo ""
 echo "Renaming package directories"
-for versionedDir in `find ${SRC_DIR} -type d -name "*${oldMajorVersion}"`; do 
+for versionedDir in `find ${SRC_DIR_API} -type d -name "*${oldMajorVersion}"`; do 
     newDir=`echo "${versionedDir}" | sed "s/${oldMajorVersion}$/${newMajorVersion}/"`
     echo "renaming directory ${versionedDir} to ${newDir}"; 
     git mv "${versionedDir}" "${newDir}"
 done
 
 echo ""
-echo "Re-versioning ${PACKAGE_PREFIX_QUERY_API} packages"
-echo "About to change `find ${SRC_DIR} -type f -name "*.java" | xargs grep "${PACKAGE_PREFIX_QUERY_API}.${oldMajorVersion}" | wc -l` lines"
-find ${SRC_DIR} -type f -name "*.java" | xargs sed -i "s/${PACKAGE_PREFIX_QUERY_API}\.${oldMajorVersion}/${PACKAGE_PREFIX_QUERY_API}.${newMajorVersion}/g"
+echo "Re-versioning ${PACKAGE_PREFIX_QUERY_API} package references in 'stroom-query-api' code"
+echo "About to change `find ${SRC_DIR_API} -type f -name "*.java" | xargs grep "${PACKAGE_PREFIX_QUERY_API}.${oldMajorVersion}" | wc -l` lines"
+find ${SRC_DIR_API} -type f -name "*.java" | xargs sed -i "s/${PACKAGE_PREFIX_QUERY_API}\.${oldMajorVersion}/${PACKAGE_PREFIX_QUERY_API}.${newMajorVersion}/g"
 
 echo ""
-echo "Re-versioning ${PACKAGE_PREFIX_DATASOURCE_API} packages"
-echo "About to change `find ${SRC_DIR} -type f -name "*.java" | xargs grep "${PACKAGE_PREFIX_DATASOURCE_API}.${oldMajorVersion}" | wc -l` lines"
-find ${SRC_DIR} -type f -name "*.java" | xargs sed -i "s/${PACKAGE_PREFIX_DATASOURCE_API}\.${oldMajorVersion}/${PACKAGE_PREFIX_DATASOURCE_API}.${newMajorVersion}/g"
+echo "Re-versioning ${PACKAGE_PREFIX_QUERY_API} package references in 'stroom-query-common' code"
+echo "About to change `find ${SRC_DIR_COMMON} -type f -name "*.java" | xargs grep "${PACKAGE_PREFIX_QUERY_API}.${oldMajorVersion}" | wc -l` lines"
+find ${SRC_DIR_COMMON} -type f -name "*.java" | xargs sed -i "s/${PACKAGE_PREFIX_QUERY_API}\.${oldMajorVersion}/${PACKAGE_PREFIX_QUERY_API}.${newMajorVersion}/g"
+
+echo ""
+echo "Re-versioning ${PACKAGE_PREFIX_DATASOURCE_API} package references in 'stroom-query-api' code"
+echo "About to change `find ${SRC_DIR_API} -type f -name "*.java" | xargs grep "${PACKAGE_PREFIX_DATASOURCE_API}.${oldMajorVersion}" | wc -l` lines"
+find ${SRC_DIR_API} -type f -name "*.java" | xargs sed -i "s/${PACKAGE_PREFIX_DATASOURCE_API}\.${oldMajorVersion}/${PACKAGE_PREFIX_DATASOURCE_API}.${newMajorVersion}/g"
+
+echo ""
+echo "Re-versioning ${PACKAGE_PREFIX_DATASOURCE_API} package references in 'stroom-query-common' code"
+echo "About to change `find ${SRC_DIR_COMMON} -type f -name "*.java" | xargs grep "${PACKAGE_PREFIX_DATASOURCE_API}.${oldMajorVersion}" | wc -l` lines"
+find ${SRC_DIR_COMMON} -type f -name "*.java" | xargs sed -i "s/${PACKAGE_PREFIX_DATASOURCE_API}\.${oldMajorVersion}/${PACKAGE_PREFIX_DATASOURCE_API}.${newMajorVersion}/g"
 
 
 echo ""
