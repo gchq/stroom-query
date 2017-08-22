1973204bb5ee52f7b64050d4e26b0d8d473faa6c
at055612 <at055612@gmail.com>
2017-08-22 07:37:42 +0100

    Tidy up javadoc

--- stroom-query-common/src/main/java/stroom/query/common/v2/Store.java
+++ stroom-query-common/src/main/java/stroom/query/common/v2/Store.java
@@ -61,7 +61,8 @@ public interface Store {
      * Get a list of the default maximum result row counts for each grouping level,
      * e.g. [100,10,1] means at most 100 rows for group level 0, 10 for each group level
      * 1 and 1 for each group level 2
-     * @return
+     * @return A list of {@link Integer} values, where the value represents the default
+     * maximum number of results at a grouping depth equal to the position in the list
      */
    List<Integer> getDefaultMaxResultsSizes();
 
