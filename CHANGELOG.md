8c2a4a922b4f47b9f44f5212a72b6f622fa41b51
at055612 <at055612@gmail.com>
2017-08-23 15:19:41 +0100

    Add javadoc & get() method to ExpressionBuilder

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ### Added
 
+* Add ExpressionBuilder.end() method for better chaining of methods.
 
 ### Changed
 
--- stroom-query-api/src/main/java/stroom/query/api/v2/ExpressionBuilder.java
+++ stroom-query-api/src/main/java/stroom/query/api/v2/ExpressionBuilder.java
@@ -24,13 +24,24 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * A builder class for constructing a tree of {@link ExpressionItem expressionItems}
+ * A builder class for constructing a tree of {@link ExpressionItem expressionItems}. Builders can be nested as follows:
+ * <pre>
+ ExpressionOperator root = new ExpressionBuilder(Op.AND)
+     .addTerm("fieldX", Condition.EQUALS, "abc")
+     .addOperator(Op.OR)
+         .addTerm("fieldA", Condition.EQUALS, "Fred")
+         .addTerm("fieldA", Condition.EQUALS, "Fred")
+         .end()
+     .addTerm("fieldY", Condition.BETWEEN, "10,20")
+     .build();
+ * </pre>
  */
 public final class ExpressionBuilder {
 
     private final Boolean enabled;
     private final Op op;
     private final List<Object> children = new ArrayList<>();
+    private final ExpressionBuilder parentBuilder;
 
     /**
      * Create a builder with an enabled 'AND' operator as its root item
@@ -49,16 +60,30 @@ public final class ExpressionBuilder {
     }
 
     /**
-     * Create a builder with the supplied {@link ExpressionOperator} type as its root item. The root item will be
-     * enabled
+     * Create a builder with the supplied {@link Op} as the type of its root {@link ExpressionOperator}.
+     * The root item will be enabled.
      * @param enabled The enabled state of the root operator item. A value of null is take to mean enabled
      * @param op The {@link Op operator type} of the root operator
      */
     public ExpressionBuilder(final Boolean enabled, final Op op) {
-        this.enabled = enabled;
-        this.op = op;
+        this(enabled, op, null);
     }
 
+    private ExpressionBuilder(final Boolean enabled, final Op op, final ExpressionBuilder parentBuilder) {
+        this.enabled = enabled;
+        this.op = op;
+        this.parentBuilder = parentBuilder;
+    }
+
+    /**
+     * Add a value term to the builder, e.g fieldX|BETWEEN|10,100
+     * Term is enabled by default.
+     * Not all conditions are supported by all data sources
+     * @param field The field in the data source that is being evaluated
+     * @param condition The {@link Condition} of the predicate
+     * @param value The value for the predicate
+     * @return This builder instance that the term is being added to
+     */
     public ExpressionBuilder addTerm(final String field,
                                      final Condition condition,
                                      final String value) {
@@ -66,6 +91,15 @@ public final class ExpressionBuilder {
         return addTerm(null, field, condition, value, null);
     }
 
+    /**
+     * Add a value term to the builder, e.g fieldX|BETWEEN|10,100
+     * Not all conditions are supported by all data sources
+     * @param enabled Sets the terms state to enabled if true or null, disabled if false
+     * @param field The field in the data source that is being evaluated
+     * @param condition The {@link Condition} of the predicate
+     * @param value The value for the predicate
+     * @return This builder instance that the term is being added to
+     */
     public ExpressionBuilder addTerm(final Boolean enabled,
                                      final String field,
                                      final Condition condition,
@@ -74,6 +108,15 @@ public final class ExpressionBuilder {
         return addTerm(enabled, field, condition, value, null);
     }
 
+    /**
+     * Add a dictionary term to the builder, e.g fieldX|IN_DICTIONARY|docRefToDictionaryY
+     * Term is enabled by default. Not all data sources support dictionary terms and only certain
+     * conditions are supported for a dictionary term.
+     * @param field The field in the data source that is being evaluated against the dictionary
+     * @param condition The {@link Condition} of the predicate
+     * @param dictionary The {@link DocRef} of the dictionary entity in stroom
+     * @return This builder instance that the term is being added to
+     */
     public ExpressionBuilder addDictionaryTerm(final String field,
                                                final Condition condition,
                                                final DocRef dictionary) {
@@ -81,6 +124,16 @@ public final class ExpressionBuilder {
         return addTerm(null, field, condition, null, dictionary);
     }
 
+    /**
+     * Add a dictionary term to the builder, e.g fieldX|IN_DICTIONARY|docRefToDictionaryY
+     * Term is enabled by default. Not all data sources support dictionary terms and only certain
+     * conditions are supported for a dictionary term.
+     * @param enabled Sets the terms state to enabled if true or null, disabled if false
+     * @param field The field in the data source that is being evaluated against the dictionary
+     * @param condition The {@link Condition} of the predicate
+     * @param dictionary The {@link DocRef} of the dictionary entity in stroom
+     * @return This builder instance that the term is being added to
+     */
     public ExpressionBuilder addDictionaryTerm(final Boolean enabled,
                                                final String field,
                                                final Condition condition,
@@ -89,7 +142,9 @@ public final class ExpressionBuilder {
         return addTerm(enabled, field, condition, null, dictionary);
     }
 
-    public ExpressionBuilder addTerm(final Boolean enabled, final String field,
+    //TODO should this be public? Better to use an addTerm for addDictotionaryTerm from above?
+    public ExpressionBuilder addTerm(final Boolean enabled,
+                                     final String field,
                                      final Condition condition,
                                      final String value,
                                      final DocRef dictionary) {
@@ -98,6 +153,11 @@ public final class ExpressionBuilder {
         return this;
     }
 
+    /**
+     * Adds an {@link ExpressionTerm} to this builder
+     * @param term The {@link ExpressionTerm} to add to this builder
+     * @return This builder instance that the term is being added to
+     */
     public ExpressionBuilder addTerm(final ExpressionTerm term) {
         if (term != null) {
             addTerm(term.getEnabled(), term.getField(), term.getCondition(), term.getValue(), term.getDictionary());
@@ -106,16 +166,36 @@ public final class ExpressionBuilder {
         return this;
     }
 
+    /**
+     * Adds an {@link ExpressionOperator} of type {@link Op} to this expression builder.
+     * The {@link ExpressionOperator} will be enabled by default.
+     * @param op The type of the {@link ExpressionOperator} to add
+     * @return A new builder instance for the added {@link ExpressionOperator} to allow adding items to this
+     * {@link ExpressionOperator}
+     */
     public ExpressionBuilder addOperator(final Op op) {
         return addOperator(null, op);
     }
 
+    /**
+     * Adds an {@link ExpressionOperator} of type {@link Op} to this expression builder.
+     * The {@link ExpressionOperator} will be enabled by default.
+     * @param enabled Sets the terms state to enabled if true or null, disabled if false
+     * @param op The type of the {@link ExpressionOperator} to add
+     * @return A new builder instance for the added {@link ExpressionOperator} to allow adding items to this
+     * {@link ExpressionOperator}
+     */
     public ExpressionBuilder addOperator(final Boolean enabled, final Op op) {
-        final ExpressionBuilder builder = new ExpressionBuilder(enabled, op);
+        final ExpressionBuilder builder = new ExpressionBuilder(enabled, op, this);
         children.add(builder);
         return builder;
     }
 
+    /**
+     * Adds the passed {@link ExpressionOperator} tree to this builder
+     * @param operator The {@link ExpressionOperator} to add
+     * @return A new builder instance for the added {@link ExpressionOperator}
+     */
     public ExpressionBuilder addOperator(final ExpressionOperator operator) {
         if (operator != null) {
             final ExpressionBuilder builder = addOperator(operator.getEnabled(), operator.getOp());
@@ -155,5 +235,19 @@ public final class ExpressionBuilder {
         }
 
         return new ExpressionOperator(enabled, op, list);
+    }
+
+    /**
+     * If this builder instance was created as a child of another builder instance then this method returns the
+     * parent builder instance so you can continue to add items to the parent. See the example in
+     * {@link ExpressionBuilder}
+     * @return The parent builder instance if there is one, else returns this builder instance
+     */
+    public ExpressionBuilder end() {
+        if (parentBuilder != null) {
+            return parentBuilder;
+        } else {
+            return this;
+        }
     }
 }
