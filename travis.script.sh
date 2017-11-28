b904b4d44910bc4aeb96e6bbde6f1fe7ac8b6d37
at055612 <at055612@gmail.com>
2017-11-28 08:48:18 +0000

    gh-6 Fix variable export

--- travis.script.sh
+++ travis.script.sh
@@ -11,17 +11,15 @@ YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m' # No Colour 
 
-#establish what version of stroom we are building
+#establish what version of stroom we are building, export it to make it 
+#available to other scripts run by travis
 if [ -n "$TRAVIS_TAG" ]; then
     #Tagged commit so use that as our stroom version, e.g. v6.0.0
-    STROOM_QUERY_VERSION="${TRAVIS_TAG}"
+    export STROOM_QUERY_VERSION="${TRAVIS_TAG}"
 else
     #No tag so use the branch name as the version, e.g. dev-SNAPSHOT
-    STROOM_QUERY_VERSION="${TRAVIS_BRANCH}-SNAPSHOT"
+    export STROOM_QUERY_VERSION="${TRAVIS_BRANCH}-SNAPSHOT"
 fi
-
-#Export this so other scripts can see it
-export STROOM_QUERY_VERSION
 
 #Dump all the travis env vars to the console for debugging
 echo -e "TRAVIS_BUILD_NUMBER:  [${GREEN}${TRAVIS_BUILD_NUMBER}${NC}]"
