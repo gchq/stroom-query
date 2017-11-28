59d92f4bd181f8a506b03314892f2b111a0c2b01
at055612 <at055612@gmail.com>
2017-11-28 16:56:11 +0000

    gh-6 Remove unused files/code from travis approach

--- travis.script.sh
+++ travis.script.sh
@@ -11,22 +11,19 @@ YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m' # No Colour 
 
-#establish what version of stroom we are building, export it to make it 
-#available to other scripts run by travis
+#establish what version we are building
 if [ -n "$TRAVIS_TAG" ]; then
-    #Tagged commit so use that as our stroom version, e.g. v6.0.0
-    STROOM_QUERY_VERSION="${TRAVIS_TAG}"
+    #Tagged commit so use that as our version, e.g. v1.2.3
+    PRODUCT_VERSION="${TRAVIS_TAG}"
 
     #upload the maven artefacts to bintray
     EXTRA_BUILD_ARGS="bintrayUpload"
 else
     #No tag so use the branch name as the version, e.g. dev-SNAPSHOT
-    STROOM_QUERY_VERSION="${TRAVIS_BRANCH}-SNAPSHOT"
+    #None tagged builds are NOT pushed to bintray
+    PRODUCT_VERSION="${TRAVIS_BRANCH}-SNAPSHOT"
     EXTRA_BUILD_ARGS=""
 fi
-
-#Exporting variables does not seem to work across scripts so do it via a file
-echo "export STROOM_QUERY_VERSION=${STROOM_QUERY_VERSION}" >> env.sh
 
 #Dump all the travis env vars to the console for debugging
 echo -e "TRAVIS_BUILD_NUMBER:  [${GREEN}${TRAVIS_BUILD_NUMBER}${NC}]"
@@ -35,8 +32,7 @@ echo -e "TRAVIS_BRANCH:        [${GREEN}${TRAVIS_BRANCH}${NC}]"
 echo -e "TRAVIS_TAG:           [${GREEN}${TRAVIS_TAG}${NC}]"
 echo -e "TRAVIS_PULL_REQUEST:  [${GREEN}${TRAVIS_PULL_REQUEST}${NC}]"
 echo -e "TRAVIS_EVENT_TYPE:    [${GREEN}${TRAVIS_EVENT_TYPE}${NC}]"
-echo -e "STROOM_QUERY_VERSION: [${GREEN}${STROOM_QUERY_VERSION}${NC}]"
-
+echo -e "PRODUCT_VERSION:      [${GREEN}${PRODUCT_VERSION}${NC}]"
 
 #Run the build (including running maven install task to generate poms
-./gradlew -Pversion=$STROOM_QUERY_VERSION clean build install ${EXTRA_BUILD_ARGS}
+./gradlew -Pversion=$PRODUCT_VERSION clean build ${EXTRA_BUILD_ARGS}
