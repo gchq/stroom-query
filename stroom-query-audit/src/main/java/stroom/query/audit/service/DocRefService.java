fe601a3c43dfc48ac0261f1975b20457021ef5ee
stroomdev66 <stroomdev66@gmail.com>
2018-03-27 17:29:40 +0100

    Remove generic catch Exception

--- stroom-query-audit/src/main/java/stroom/query/audit/service/DocRefService.java
+++ stroom-query-audit/src/main/java/stroom/query/audit/service/DocRefService.java
@@ -12,37 +12,39 @@ import java.util.Optional;
 
 /**
  * Generic form of DocRef service, templated to the class the encapsulates the DocRef
+ *
  * @param <T> The class that represents the full document
  */
-public interface DocRefService <T extends DocRefEntity> {
+public interface DocRefService<T extends DocRefEntity> {
     /**
      * Get the doc ref type that this service wraps.
+     *
      * @return The doc ref type name
      */
     String getType();
 
     /**
      * Retrieve all of the index entities currently registered
+     *
      * @param user The logged in user
-     * @if anything goes wrong
      * @return The list of all known index entities
      */
-    List<T> getAll(ServiceUser user) ;
+    List<T> getAll(ServiceUser user);
 
     /**
      * Retrieve the full config for the given DocRef
+     *
      * @param user The logged in user
-     * @param uuid              The UUID of the docRef to return
-     * @if anything goes wrong
-     * @return                  The full implementation specific config for this docRef.
+     * @param uuid The UUID of the docRef to return
+     * @return The full implementation specific config for this docRef.
      */
-    Optional<T> get(ServiceUser user, String uuid) ;
+    Optional<T> get(ServiceUser user, String uuid);
 
     /**
      * Retrieve the info about a doc ref
+     *
      * @param user The logged in user
      * @param uuid The UUID of the doc ref to find
-     * @if anything goes wrong
      * @return The DocRefInfo for the UUID
      */
     default Optional<DocRefInfo> getInfo(ServiceUser user, String uuid) {
@@ -63,46 +65,43 @@ public interface DocRefService <T extends DocRefEntity> {
      * A new document has been created in Stroom
      *
      * @param user The logged in user
-     * @param uuid              The UUID of the document as created by stroom
-     * @param name              The name of the document to be created.
-     * @if anything goes wrong
+     * @param uuid The UUID of the document as created by stroom
+     * @param name The name of the document to be created.
      * @return The new index entity
      */
-    Optional<T> createDocument(ServiceUser user, String uuid, String name) ;
+    Optional<T> createDocument(ServiceUser user, String uuid, String name);
 
     /**
      * Used to update a specific document.
      * This will be used by our user interface to configure the underlying index and the stroom references to it
-     * @param user The logged in user
-     * @param uuid The UUID of DocRef used to store the index configuration
+     *
+     * @param user          The logged in user
+     * @param uuid          The UUID of DocRef used to store the index configuration
      * @param updatedConfig The updated configuration
-     * @if anything goes wrong
      * @return The updated config
      */
-    Optional<T> update(ServiceUser user, String uuid, T updatedConfig) ;
+    Optional<T> update(ServiceUser user, String uuid, T updatedConfig);
 
     /**
      * A notification from Stroom that a document is being copied. The external system should
      * copy it's configuration for the original into a new entity.
      *
-     * @param user The logged in user
-     * @param originalUuid      The uuid of the document being copied
-     * @param copyUuid          The uuid of the copy
-     * @if anything goes wrong
+     * @param user         The logged in user
+     * @param originalUuid The uuid of the document being copied
+     * @param copyUuid     The uuid of the copy
      * @return The new index entity
      */
-    Optional<T> copyDocument(ServiceUser user, String originalUuid, String copyUuid) ;
+    Optional<T> copyDocument(ServiceUser user, String originalUuid, String copyUuid);
 
     /**
      * A Notification from Stroom that the document has been 'moved'. In most cases the external system
      * will not care about this.
      *
      * @param user The logged in user
-     * @param uuid             The uuid of the document that was moved
-     * @if anything goes wrong
+     * @param uuid The uuid of the document that was moved
      * @return The updated index entity
      */
-    Optional<T> moveDocument(ServiceUser user, String uuid) ;
+    Optional<T> moveDocument(ServiceUser user, String uuid);
 
     /**
      * A notifiation from Stroom that the name of a document has been changed. Whilst the name belongs to stroom
@@ -112,43 +111,41 @@ public interface DocRefService <T extends DocRefEntity> {
      * @param user The logged in user
      * @param uuid The uuid of the document you want to rename.
      * @param name The new name of the document.
-     * @if anything goes wrong
      * @return The updated index entity
      */
-    Optional<T> renameDocument(ServiceUser user, String uuid, String name) ;
+    Optional<T> renameDocument(ServiceUser user, String uuid, String name);
 
     /**
      * The document with this UUID is being deleted in Stroom.
      *
      * @param user The logged in user
      * @param uuid The uuid of the document you want to delete.
-     * @if anything goes wrong
      * @return Optional boolean, if missing, the document could not be found, if false, it could not be deleted
      */
-    Optional<Boolean> deleteDocument(ServiceUser user, String uuid) ;
+    Optional<Boolean> deleteDocument(ServiceUser user, String uuid);
 
     /**
      * Used to export the full details of a document for transfer.
+     *
      * @param user The logged in user
      * @param uuid The UUID of the document to export
-     * @if anything goes wrong
      * @return The exported data
      */
-    ExportDTO exportDocument(ServiceUser user, String uuid) ;
+    ExportDTO exportDocument(ServiceUser user, String uuid);
 
     /**
      * Used to import a document into the system
-     * @param user The logged in user
-     * @param uuid The UUID of the document to import
-     * @param name The Name of the document to import
+     *
+     * @param user      The logged in user
+     * @param uuid      The UUID of the document to import
+     * @param name      The Name of the document to import
      * @param confirmed Used to indicate if this is a dry run
-     * @param dataMap The data that gives all the implementation specific details
-     * @if anything goes wrong
+     * @param dataMap   The data that gives all the implementation specific details
      * @return The imported document
      */
     Optional<T> importDocument(ServiceUser user,
                                String uuid,
                                String name,
                                Boolean confirmed,
-                               Map<String, String> dataMap) ;
+                               Map<String, String> dataMap);
 }
