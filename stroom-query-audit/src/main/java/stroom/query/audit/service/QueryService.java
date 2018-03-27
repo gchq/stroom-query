5203e89904c53b15d381c744b20f722ae63ff40f
stroomdev66 <stroomdev66@gmail.com>
2018-03-27 17:28:31 +0100

    Remove generic catch Exception

--- stroom-query-audit/src/main/java/stroom/query/audit/service/QueryService.java
+++ stroom-query-audit/src/main/java/stroom/query/audit/service/QueryService.java
@@ -14,45 +14,40 @@ public interface QueryService {
      * Get the details of the DataSource given by the DocRef.
      * Used to build user interfaces for querying the specific data.
      *
-     * @param user The authenticated user
+     * @param user   The authenticated user
      * @param docRef The Doc Ref of the DataSource to fetch
      * @return The DataSource definition for the given doc ref
-     * @If something goes wrong
      */
     Optional<DataSource> getDataSource(ServiceUser user,
-                                       DocRef docRef) ;
+                                       DocRef docRef);
 
     /**
      * Conduct a search on the data, it may be a successive call for long running searches.
      *
-     * @param user The authenticated user
+     * @param user    The authenticated user
      * @param request The details of the search
-     * @return The search results
-     * @If something goes wrong
      */
     Optional<SearchResponse> search(ServiceUser user,
-                                    SearchRequest request) ;
+                                    SearchRequest request);
 
     /**
      * Destroy any existing query being conducted under the given key.
      *
-     * @param user The authenticated user
+     * @param user     The authenticated user
      * @param queryKey The query key that was given with the search request.
      * @return Success indicator
-     * @If something goes wrong
      */
     Boolean destroy(ServiceUser user,
-                    QueryKey queryKey) ;
+                    QueryKey queryKey);
 
     /**
      * Used by REST layer to retrieve the doc ref for a given query key.
      * Primarily used to check the permissions for the given doc ref.
      *
-     * @param user The authenticated user
+     * @param user     The authenticated user
      * @param queryKey The query key, it should match a current query
      * @return The DocRef of the query, if found, if not found the result will be empty.
-     * @If something goes wrong
      */
     Optional<DocRef> getDocRefForQueryKey(ServiceUser user,
-                                          QueryKey queryKey) ;
+                                          QueryKey queryKey);
 }
