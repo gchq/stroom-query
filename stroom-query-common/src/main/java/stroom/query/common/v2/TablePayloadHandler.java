67e05426a2913b0a69172460d6a1e423a59953e0
at055612 <at055612@gmail.com>
2018-03-28 13:54:34 +0100

    Fix handling of InterruptedException in TablePayloadHandler

--- stroom-query-common/src/main/java/stroom/query/common/v2/TablePayloadHandler.java
+++ stroom-query-common/src/main/java/stroom/query/common/v2/TablePayloadHandler.java
@@ -71,12 +71,17 @@ public class TablePayloadHandler implements PayloadHandler {
                 // merging.
                 try {
                     pendingMerges.put(newQueue);
-                } catch (final InterruptedException | RuntimeException e) {
+                } catch (final InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    LOGGER.warn("Thread interrupted while trying to put an item onto pendingMerges queue");
+                } catch (final RuntimeException e) {
                     LOGGER.error(e.getMessage(), e);
                 }
 
-                // Try and merge all of the items on the pending merge queue.
-                mergePending(hasTerminate);
+                if (!Thread.currentThread().isInterrupted()) {
+                    // Try and merge all of the items on the pending merge queue.
+                    mergePending(hasTerminate);
+                }
             }
         }
     }
