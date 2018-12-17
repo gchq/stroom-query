6026ca68ac38ce2df07f7832a39e2fb53ef5e17e
at055612 <at055612@gmail.com>
2018-12-17 06:50:43 +0000

    Add missing `-e` to echo in tag_release.sh

--- tag_release.sh
+++ tag_release.sh
@@ -119,7 +119,7 @@ main() {
         echo
     else
         echo
-        echo "${GREEN}Exiting without tagging a commit${NC}"
+        echo -e "${GREEN}Exiting without tagging a commit${NC}"
         echo
         exit 0
     fi
