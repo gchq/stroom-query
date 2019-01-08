3ff23e5e24921b036a2abc12251a2c5935a8c535
GCHQ-11 <gchq.developer.11@gmail.com>
2019-01-08 14:33:27 +0000

    starting the DB container once for every test

--- stroom-query-testing/src/main/java/stroom/query/testing/DatabaseContainerExtension.java
+++ stroom-query-testing/src/main/java/stroom/query/testing/DatabaseContainerExtension.java
@@ -5,17 +5,20 @@ import org.testcontainers.containers.MySQLContainer;
 import java.util.List;
 
 public class DatabaseContainerExtension {
-    public static final int MYSQL_EXPOSED_PORT = 14450;
+    private static final int MYSQL_EXPOSED_PORT = 14450;
 
-    public MySQLContainer dbContainer = new MySQLContainer();
+    private static MySQLContainer dbContainer = new MySQLContainer();
 
     void beforeAll() {
-        dbContainer.setPortBindings(List.of(String.format("%d:%d", MYSQL_EXPOSED_PORT, MySQLContainer.MYSQL_PORT)));
-        dbContainer.start();
+        if (!dbContainer.isRunning()) {
+            dbContainer.setPortBindings(List.of(String.format("%d:%d", MYSQL_EXPOSED_PORT, MySQLContainer.MYSQL_PORT)));
+            dbContainer.start();
+        }
     }
 
     void afterAll() {
-        dbContainer.stop();
+        // There doesn't seem to be a way to know if this is the 'last test', but the container seems to get cleaned up anyway...
+        //dbContainer.stop();
     }
 
     public MySQLContainer getDbContainer() {
